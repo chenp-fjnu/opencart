7b14ecf73be238273c89498773ba7245383cbdbc
Ping.Chen <chp0524@gmail.com>
2024-03-30 10:34:27 +0800

    git init

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart3/admin/');
+define('HTTP_CATALOG', 'http://opencart3/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart3/admin/');
+define('HTTPS_CATALOG', 'http://opencart3/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/phpstudy_pro/WWW/opencart-3.0.3.9/admin/');
+define('DIR_SYSTEM', 'D:/phpstudy_pro/WWW/opencart-3.0.3.9/system/');
+define('DIR_IMAGE', 'D:/phpstudy_pro/WWW/opencart-3.0.3.9/image/');
+define('DIR_STORAGE', 'D:/phpstudy_pro/WWW/storage/');
+define('DIR_CATALOG', 'D:/phpstudy_pro/WWW/opencart-3.0.3.9/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '1qaz2wsx');
+define('DB_DATABASE', 'opencart3');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
